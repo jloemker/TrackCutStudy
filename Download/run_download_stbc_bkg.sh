ffb488c05fb7d920d2cc6a58523ba88026de13de
Johanna Lomker <jloemker@yahoo.de>
2024-02-09 14:27:45 +0100

    reduce useless stuff

--- Download/run_download_stbc_bkg.sh
+++ Download/run_download_stbc_bkg.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 #enter the O2physics env manually before going into screen mode by executing this script !
-
-#source /cvmfs/alice.cern.ch/bin/alienv enter VO_ALICE@O2Physics::daily-20231108-0200-1
-
 screen -S download -d -m bash download_data_stbc.sh
