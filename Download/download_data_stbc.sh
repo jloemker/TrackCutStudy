7c2473801ffa681087d113b66d5c50cbf63f01de
Johanna Lomker <jloemker@yahoo.de>
2024-03-01 19:54:19 +0100

    remove merging section

--- Download/download_data_stbc.sh
+++ Download/download_data_stbc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-store="/dcache/alice/jlomker/LHC22_pass4_highIR_sampling" # make this your path
+store="/dcache/alice/jlomker/LHC22_pass4_lowIR" # make this your path
 
 mkdir -p $store
 
@@ -20,10 +20,13 @@ do
     [[ $line == *'#'* ]] && continue
     mkdir -p $store/$i/$j/
     alien_cp $line file:$store/$i/$j/AO2D.root
-    echo "$j/AO2D.root" >> merge_per_run.txt # we don't need the $store and $i (runnumber) here because we enter them before we execute the merging script where we find one  merge_per_run.txt per runnumber
+    echo "$j/AO2D.root" >> merge_per_run.txt # we don't need the $store and $i (runnumber) here because we enter them before we execute the merging script where we find one  merge_per_run.txt per runnumbe
     ((j+=1))
   done < files_per_run.txt
   echo "$i/AO2D.root" >> merge_runs.txt
+  #rm -rf $store/$i/merge_per_run.txt
+  #rm -rf $store/$i/files_per_run.txt
+  #rm -rf $store/$i/AO2D.root
   mv files_per_run.txt $store/$i/files_per_run.txt
   mv merge_per_run.txt $store/$i/merge_per_run.txt
 done
@@ -32,17 +35,4 @@ mv merge_runs.txt $store/merge_runs.txt
 
 echo "all files downloaded"
 
-for i in "${runs[@]}"
-do
-  cd $store/$i/ # entering the location we we store the AO2D's from the download 
-  o2-aod-merger --input merge_per_run.txt --skip-parent-files-list --skip-non-existing-files # execute the merging with the file per run 
-  echo "we might have just merged run $i" # and this is just to see where we are.. no guarentee that it works
-done
-
 cd $store # go where we find the merge_runs.txt and do it !
-
-o2-aod-merger --input merge_runs.txt --skip-parent-files-list --skip-non-existing-files # and here we run the final merge - could be that this does not work, but the merger has the --help option too !
-mv AO2D.root AnalysisResults_trees.root # renaming into the _trees.root such that we only need to move this into the proper ../Results/LHC_Period/ 
-
-echo "all done and merged into AnalysisResults_trees.root ! (or merging did not work)" # if the merging into one AO2D doesn't work, then look at the ../CutVariations/runCatvar.sh and extent the list.txt
-echo "don't mess up moving the results to their location for the cutvariation study !" # good to move all submerged directories into a backup !
