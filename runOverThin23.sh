d7870e7880a217a867f0554c44315f66372700c0
Johanna Lömker <jloemker@yahoo.de>
2024-12-05 14:02:03 +0100

    adding mini script to eun over all datasets from LHC23 while watching apw

---
+++ runOverThin23.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+datasets=(
+    "Thinner_LHC23f"
+    "Thinner_LHC23g"
+    "Thinner_LHC23h"
+    "Thinner_LHC23j"
+    "Thinner_LHC23k"
+    "Thinner_LHC23l"
+    "Thinner_LHC23m"
+    "Thinner_LHC23n"
+    "Thinner_LHC23o"
+    "Thinner_LHC23q"
+    "Thinner_LHC23r"
+    "Thinner_LHC23s"
+    "Thinner_LHC23t"
+    "Thinner_LHC23u"
+    "Thinner_LHC23v"
+    "Thinner_LHC23y"
+    "Thinner_LHC23z"
+    "Thinner_LHC23zb"
+    "Thinner_LHC23zc"
+    "Thinner_LHC23zd"
+    "Thinner_LHC23ze"
+    "Thinner_LHC23zf"
+    "Thinner_LHC23zg"
+    "Thinner_LHC23zh"
+    "Thinner_LHC23zi"
+    "Thinner_LHC23zj"
+    "Thinner_LHC23zk"
+    "Thinner_LHC23zm"
+    "Thinner_LHC23zn"
+    "Thinner_LHC23zq"
+    "Thinner_LHC23zr"
+    "Thinner_LHC23zt"
+)
+
+for dataset in "${datasets[@]}"; do
+    echo "Dataset: $dataset"
+    ./processResults.py --Mode FULL --Input Results/$dataset/AnalysisResults.root --Save True --Suffix WoPtEta --DataSet $dataset --Scaled INT
+    ./processResults.py --Mode FULL --Input Results/$dataset/AnalysisResults.root --Save True --Suffix WoPtEta --DataSet $dataset --Scaled LOG
+    ./processResults.py --Mode FULL --Input Results/$dataset/AnalysisResults.root --Save True --DataSet $dataset --Scaled INT
+    ./processResults.py --Mode FULL --Input Results/$dataset/AnalysisResults.root --Save True --DataSet $dataset --Scaled Log
+
+done
